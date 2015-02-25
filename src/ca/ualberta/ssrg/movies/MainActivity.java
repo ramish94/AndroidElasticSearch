628f1c57f875da09c9be6d16e8e1430cdbccbd0d
ramish94 <ramish@ualberta.ca>
2015-02-24 18:38:25 -0700

    Added code for searching through movies

--- src/ca/ualberta/ssrg/movies/MainActivity.java
+++ src/ca/ualberta/ssrg/movies/MainActivity.java
@@ -84,6 +84,9 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onResume() {
 		super.onResume();
+		
+		Thread thread = new SearchThread("dark");
+		thread.start();
 
 		// Refresh the list when visible
 		// TODO: Search all
@@ -125,7 +128,17 @@ public class MainActivity extends Activity {
 
 
 	class SearchThread extends Thread {
-		// TODO: Implement search thread
+		
+		private String search;
+		
+		public SearchThread(String search) {
+			this.search = search;
+		}
+		public void run() {
+			movies.addAll(movieManager.searchMovies(search, null));
+			
+			runOnUiThread(doUpdateGUIList);
+		}
 		
 	}
 
